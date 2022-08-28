e291b0ee7774796ca8156282a9776288c6d500d5
keerthana <keerthanakr555@gmail.com>
2022-08-27 20:17:36 -0700

    changed by keerthna

---
+++ keertana_extended/src/test/java/pojoCollection/ExtendedprofileoftheAutonomousApplicationTests.java
@@ -0,0 +1,13 @@
+package pojoCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendedprofileoftheAutonomousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
